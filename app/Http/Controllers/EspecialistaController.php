3d912fb4d9c129a6a798d060269dc39fc8c8f01c
DAW <juanmiguel.valle@iesdonana.org>
2025-05-08 16:27:12 +0200

    carga inicial

---
+++ app/Http/Controllers/EspecialistaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEspecialistaRequest;
+use App\Http\Requests\UpdateEspecialistaRequest;
+use App\Models\Especialista;
+
+class EspecialistaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEspecialistaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Especialista $especialista)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Especialista $especialista)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEspecialistaRequest $request, Especialista $especialista)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Especialista $especialista)
+    {
+        //
+    }
+}
