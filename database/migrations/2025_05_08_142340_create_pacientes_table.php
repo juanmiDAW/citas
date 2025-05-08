b52c6f2879b9dcd6a89b37595095428c192956fb
DAW <juanmiguel.valle@iesdonana.org>
2025-05-08 18:59:28 +0200

    modificacion de la migracion de pacientes

--- database/migrations/2025_05_08_142340_create_pacientes_table.php
+++ database/migrations/2025_05_08_142340_create_pacientes_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('pacientes', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->timestamp('reserva');
             $table->timestamps();
         });
     }
